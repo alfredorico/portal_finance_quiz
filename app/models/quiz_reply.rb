466709657e11b4a778768e298cfb19b7beca036c
Alfredo E. Rico Moros <alfredorico@gmail.com>
2017-07-26 01:48:44 -0400

    dev / Validating quiz replies (Ongoing process)

--- app/models/quiz_reply.rb
+++ app/models/quiz_reply.rb
@@ -1,4 +1,17 @@
 class QuizReply < ApplicationRecord
   belongs_to :quiz
   belongs_to :question
+  # validates :selected_answers, presence: true, allow_blank: false
+  validate :matching_question_id_with_answers_ids_selected
+
+  protected
+  def matching_question_id_with_answers_ids_selected
+    if selected_answers.present?
+      unless question.choices.pluck(:id).include?(self.selected_answers.split(','))
+        errors.add(:selected_answers,"Your selected answer is not valid")        
+      end
+    else
+      errors.add(:selected_answers,"You must choice one or more answers")
+    end
+  end
 end
