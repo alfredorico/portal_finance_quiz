813ed86ca3bcd9deeef1f4d6cfcc9177ce16bfe3
Alfredo E. Rico Moros <alfredorico@gmail.com>
2017-07-26 00:36:32 -0400

    Fix / Uncomment QuizBuilder#instantiate

--- app/services/quiz_builder.rb
+++ app/services/quiz_builder.rb
@@ -1,14 +1,14 @@
 module QuizBuilder
   extend self
   def instantiate
-    #ActiveRecord::Base.transaction do
-    #  @quiz = Quiz.create(id: generate_uniqueid)
-    #  Question.actives.each do |question|
-    #    QuizReply.create(quiz_id: @quiz.id, question: question) # For each question, create a placeholder reply that will get
-    #                                                            # answered on client side
-    #  end
-    #  @quiz
-    #end
+    ActiveRecord::Base.transaction do
+      @quiz = Quiz.create(id: generate_uniqueid)
+      Question.actives.each do |question|
+        QuizReply.create(quiz_id: @quiz.id, question: question) # For each question, create a placeholder reply that will get
+                                                                # answered on client side
+      end
+      @quiz
+    end
     nil
   end
 
