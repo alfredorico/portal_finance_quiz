527126de99ed3cf4ca6516d61ea4f1c5d12e578b
Alfredo E. Rico Moros <alfredorico@gmail.com>
2017-07-26 12:41:56 -0400

    dev / Removing unnecesary files

--- app/controllers/v1/questions_listing_controller.rb
+++
@@ -1,5 +0,0 @@
-class V1::QuestionsListingController < ApplicationController
-  def index
-    FullQuestionsListing.all
-  end
-end
--- app/services/questions_listing.rb
+++
@@ -1,8 +0,0 @@
-class QuestionsListing
-  def self.list_questions_and_choices
-    Question.includes(:choices)
-  end
-  def self.list_questions_and_choices_with_responses
-    Question.includes(:choices_with_responses)
-  end
-end
